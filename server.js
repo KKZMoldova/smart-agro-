ca39f25ad8c588392f3d372c193ac12cdc5dc20f
KKZMoldova <colin_lx@yahoo.com>
2026-05-23 10:20:41 +0300

    Update server.js

--- server.js
+++ server.js
@@ -222,7 +222,7 @@ app.get('/api/weather', auth, async (req, res) => {
     const end   = new Date();
     const start = new Date();
     start.setDate(start.getDate() - Math.min(days, 7));
-    const fcPath = `/data/normal/station/${station}/data/hourly?from=${Math.floor(start/1000)}&to=${Math.floor(end/1000)}`;
+    const fcPath = `/data/normal/station/${station}/data/last/7/hourly`;
     const fc = await fetch('https://api.fieldclimate.com/v2' + fcPath, { headers: fcHeaders('GET', fcPath) });
     if (!fc.ok) throw new Error('FC: ' + fc.status);
     const fcData = await fc.json();
